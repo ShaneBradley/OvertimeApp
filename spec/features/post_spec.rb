fef52fc99de03fa7e80f9cccd94fa6fd6d525c74
Jordan Hudgens <jordan@devcamp.com>
2018-01-03 23:37:59 +0000

    refactored factory tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-    login_as(user, :scope => :user)
+    @user = FactoryGirl.create(:user)
+    login_as(@user, :scope => :user)
   end
   
   describe 'index' do
@@ -21,10 +21,10 @@ describe 'navigate' do
     end
     
     it 'has a list of posts' do
-      post1 = @post = Post.create(date: Date.today, rationale: "Post1")
-      post1 = @post = Post.create(date: Date.today, rationale: "Post2")
+      post1 = FactoryGirl.build_stubbed(:post)
+      post1 = FactoryGirl.build_stubbed(:second_post)
       visit posts_path
-      expect(page).to have_content(/Post1|Post2/)
+      expect(page).to have_content(/Rationale|content/)
     end
   end
   
